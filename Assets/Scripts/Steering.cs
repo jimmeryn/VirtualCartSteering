616060670d7bf5e0762941a2431776483fad873f
jimmeryn <jimmeryn@gmail.com>
2020-10-07 14:01:32 +0200

    Box cast for steering

--- Assets/Scripts/Steering.cs
+++ Assets/Scripts/Steering.cs
@@ -4,30 +4,48 @@ public class Steering : MonoBehaviour {
   private GameObject target;
   private Vector3 velocity = Vector3.zero;
   private Rigidbody rigidBody;
-  private float speed = 5.0f;
+  private float speed = 50.0f;
+  private float maxHitDistance;
+  private float hitDistance;
+  private Vector3 origin;
+  private Vector3 direction;
+  private Vector3 cubeSize;
 
   private void Start() {
     target = GameObject.FindWithTag("Target");
     rigidBody = GetComponent<Rigidbody>();
-  }
-  void FixedUpdate() {
-    RaycastHit hit;
-    if (Physics.Raycast(transform.position, target.transform.position, out hit)) {
-      Debug.DrawLine(transform.position, target.transform.position);
-    }
-    rigidBody.MovePosition(rigidBody.position + velocity * Time.fixedDeltaTime);
+    maxHitDistance = CalculateRangeToTarget(transform.position, target.transform.position);
+    hitDistance = maxHitDistance;
+    cubeSize = transform.localScale;
   }
 
-  void Update() {
-    RaycastHit hit;
+  private void Update() {
+    origin = transform.position;
+    direction = (target.transform.position - transform.position).normalized;
+    maxHitDistance = (transform.position - target.transform.position).magnitude;
+  }
 
-    if (Physics.Raycast(transform.position, (target.transform.position - transform.position).normalized, out hit)) {
-      Debug.DrawLine(transform.position, target.transform.position);
-      if (hit.collider.tag == "Target") {
+  private void FixedUpdate() {
+    RaycastHit hit;
+    if (Physics.BoxCast(origin, cubeSize / 2, direction, out hit, transform.rotation)) {
+      hitDistance = hit.distance;
+      if (hit.collider.tag == "Target" || maxHitDistance < 5.0f) {
         velocity = new Vector3(target.transform.position.x - transform.position.x, 0, target.transform.position.z - transform.position.z).normalized * speed;
       } else {
+        // Using ObstacleAvoidance in this case
         velocity = Vector3.zero;
       }
+      rigidBody.MovePosition(rigidBody.position + velocity * Time.fixedDeltaTime);
     }
+  }
+
+  private void OnDrawGizmos() {
+    Gizmos.color = Color.blue;
+    Gizmos.DrawLine(origin, origin + direction * hitDistance);
+    Gizmos.DrawWireCube(origin + direction * hitDistance, cubeSize);
+  }
+
+  float CalculateRangeToTarget(Vector3 playerPosition, Vector3 targetPosition) {
+    return (targetPosition - playerPosition).magnitude;
   }
 }
