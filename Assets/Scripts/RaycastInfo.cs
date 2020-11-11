ac8191282bdb11a1957dd539ca7a6c23fc2bf960
jimmeryn <jimmeryn@gmail.com>
2020-11-11 15:52:34 +0100

    deleted angle - not used

--- Assets/Scripts/RaycastInfo.cs
+++ Assets/Scripts/RaycastInfo.cs
@@ -5,13 +5,11 @@ public struct RaycastInfo
   public bool hit;
   public Vector3 point;
   public float distance;
-  public float angle;
 
 
-  public RaycastInfo(bool _hit, Vector3 _point, float _distance, float _angle) {
+  public RaycastInfo(bool _hit, Vector3 _point, float _distance) {
     hit = _hit;
     point = _point;
     distance = _distance;
-    angle = _angle;
   }
 }
