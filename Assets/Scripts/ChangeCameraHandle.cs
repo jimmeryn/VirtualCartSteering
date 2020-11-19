4a0e542040b37aa7d37744c1015f1be8c15d836f
jimmeryn <jimmeryn@gmail.com>
2020-11-19 19:28:19 +0100

    ui buttons handlers

---
+++ Assets/Scripts/ChangeCameraHandle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ChangeCameraHandle : MonoBehaviour
+{
+  private Camera cameraVehicle;
+  private Camera cameraPlane;
+  private bool vehicleCamera = true;
+
+  public void CameraSwitch() {
+    cameraVehicle = GameObject.FindGameObjectWithTag(Tags.Player).GetComponentInChildren<Camera>();
+    cameraPlane = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    if (vehicleCamera) {
+      cameraVehicle.enabled = false;
+      cameraPlane.enabled = true;
+      vehicleCamera = false;
+    } else {
+      cameraPlane.enabled = false;
+      cameraVehicle.enabled = true;
+      vehicleCamera = true;
+    }
+  }
+}
---
+++ Assets/Scripts/RestartHandle.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartHandle : MonoBehaviour {
+  public void RestartClick() {
+    enabled = false;
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+}
