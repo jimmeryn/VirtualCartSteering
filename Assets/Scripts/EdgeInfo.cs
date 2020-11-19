434e197f4ae1c6a1542fafd6442e28b9bacb1365
jimmeryn <jimmeryn@gmail.com>
2020-11-19 19:31:23 +0100

    Edge info class used for edge detection

---
+++ Assets/Scripts/EdgeInfo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public struct EdgeInfo {
+  public Vector3 pointOn;
+  public Vector3 pointOut;
+
+  public EdgeInfo(Vector3 _pointOn, Vector3 _pointOut) {
+    pointOn = _pointOn;
+    pointOut = _pointOut;
+  }
+}
