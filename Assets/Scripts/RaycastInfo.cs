0cdafcb23af894e39eba438c49e61b1bf2b48e7d
jimmeryn <jimmeryn@gmail.com>
2020-11-19 19:30:22 +0100

    Added angle for edge detection

--- Assets/Scripts/RaycastInfo.cs
+++ Assets/Scripts/RaycastInfo.cs
@@ -5,11 +5,13 @@ public struct RaycastInfo
   public bool hit;
   public Vector3 point;
   public float distance;
+  public float angle;
 
 
-  public RaycastInfo(bool _hit, Vector3 _point, float _distance) {
+  public RaycastInfo(bool _hit, Vector3 _point, float _distance, float _angle) {
     hit = _hit;
     point = _point;
     distance = _distance;
+    angle = _angle;
   }
 }
