d28269ea9ab8e9f8d2ca6887aa9aa6231bb1a9bd
jimmeryn <jimmeryn@gmail.com>
2020-11-25 12:09:11 +0100

    A* basic algorithm

--- Assets/Scripts/AStar.cs
+++ Assets/Scripts/AStar.cs
@@ -20,88 +20,34 @@ public static class AStar
   }
 
   public static Vector3 CalculateBestNode(Vector3 currentPosition, Vector3[] nodes) {
-    //var nextNode = new Node(currentPosition, Mathf.Infinity, Mathf.Infinity);
-    //for (int i = 0; i < openList.Count - 1; i++) {
-    //  if (openList[i].f <= nextNode.f) {
-    //    nextNode = openList[i];
-    //  }
-    //}
-    //openList.Remove(nextNode);
-    //closedList.Add(nextNode);
+    Node currentPositionNode = new Node();
+    if(Functions.ListContainsNodeLocation(openList, currentPosition)) {
+      currentPositionNode = Functions.GetNodeFromListByLocation(openList, currentPosition);
+    } else {
+      currentPositionNode = Functions.GetNodeFromListByLocation(closedList, currentPosition);
+    }
 
-    //foreach (var node in nodes) {
-    //  if (closedList.Contains(new Node(node, 0, 0)))
-    //    continue;
+    foreach (var nodeLocation in nodes) {
+      if (closedList.Contains(new Node(nodeLocation, 0, 0))) {
+        continue;
+      }
+      
+      var g = currentPositionNode.g + Functions.VectorLength(currentPosition, nodeLocation);
+      var h = Functions.VectorLength(nodeLocation, target);
+      var node = new Node(nodeLocation, g, h);
 
-    //  var neighbourDistance = Functions.VectorLength(currentPosition, node);
-    //  var g = closedList.Count > 0 ? closedList[closedList.Count - 1].g + neighbourDistance : neighbourDistance;
-    //  var newNode = new Node(node, g, Functions.VectorLength(node, target));
+      if (Functions.ListContainsNodeLocation(openList, node.location)) {
+        continue;
+      }
 
-    //  if (openList.Contains(newNode) && newNode.g > FindNodeByLocation(openList, newNode.location).g)
-    //    continue;
-
-    //  openList.Add(newNode);
-    //}
-    //return nextNode.location;
+      openList.Add(node);
+    }
     var currentNode = openList.OrderBy(n => n.f).First();
     openList.Remove(currentNode);
     closedList.Add(currentNode);
     if (currentNode.location == target) {
       return currentNode.location;
     }
-    foreach(var nodeLocation in nodes) {
-      if (closedList.Contains(new Node(nodeLocation, 0, 0))) {
-        continue;
-      }
-      var g = currentNode.g + Functions.VectorLength(currentNode.location, nodeLocation);
-      var h = Functions.VectorLength(nodeLocation, target);
-      var node = new Node(nodeLocation, g, h);
-      if (openList.Contains(new Node(node.location, 0, 0))) {
-        continue;
-      }
-      openList.Add(node);
-    }
     return currentNode.location;
-  }
-
-  private static Node FindNodeByLocation(List<Node> list, Vector3 location) {
-    var node = new Node(start, 0, 0);
-    foreach(var listNode in list) {
-      if (listNode.location == location) {
-        node = listNode;
-      }
-    }
-    return node;
-  }
-
-
-
-  public class Node {
-    public Vector3 location { get; private set; }
-    public float g { get; private set; }
-    public float h { get; private set; }
-    public float f { get { return g + h; } }
-
-    public Node(Vector3 _location, float _g, float _h) {
-      location = _location;
-      g = _g;
-      h = _h;
-    }
-
-    public static bool operator ==(Node n1, Node n2) {
-      return n1.location == n2.location;
-    }
-
-    public static bool operator !=(Node n1, Node n2) {
-      return n1.location != n2.location;
-    }
-
-    public override bool Equals(object obj) {
-      return base.Equals(obj);
-    }
-
-    public override int GetHashCode() {
-      return base.GetHashCode();
-    }
   }
 }
