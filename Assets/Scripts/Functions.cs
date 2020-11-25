cc80873013bebd4aebf64a84c9a32203e306ff7f
jimmeryn <jimmeryn@gmail.com>
2020-11-25 12:11:54 +0100

    Node functionality + error calculation fix

--- Assets/Scripts/Functions.cs
+++ Assets/Scripts/Functions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public static class Functions
 {
@@ -12,5 +13,27 @@ public static class Functions
 
   public static float VectorLength(Vector3 v1, Vector3 v2) {
     return (v2 - v1).magnitude;
+  }
+
+  public static float CalculateError(float v1, float v2) {
+    return Mathf.Abs(v1 - v2);
+  }
+
+  public static bool ListContainsNodeLocation(List<Node> list, Vector3 location) {
+    foreach (var node in list) {
+      if (node.location == location) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static Node GetNodeFromListByLocation(List<Node> list, Vector3 location) {
+    foreach (var node in list) {
+      if (node.location == location) {
+        return node;
+      }
+    }
+    return new Node();
   }
 }
